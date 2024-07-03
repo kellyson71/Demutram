092745242e78866ae9557dd5a0bea0966503572b
kellyson <109356794+kellyson71@users.noreply.github.com>
2024-07-03 08:17:57 -0300

    teste commit

--- contato/script.js
+++ contato/script.js
@@ -21,13 +21,25 @@ function submitForm() {
         feedback.classList.add("success");
         feedback.classList.remove("error");
         feedback.style.display = "block";
+        
+        setTimeout(() => {
+            feedback.style.display = "none";
+        }, 3000);
     })
     .catch(error => {
         const feedback = document.getElementById("feedback");
-        feedback.innerHTML = "Ocorreu um erro ao enviar o formulário.";
+        feedback.innerHTssh-rsa
+        ML = "Ocorreu um erro ao enviar o formulário.";
         feedback.classList.add("error");
         feedback.classList.remove("success");
         feedback.style.display = "block";
+        
+        setTimeout(() => {
+            feedback.style.display = "none";
+        }, 3000);
+
+
+        console.error("Erro:", error);
         console.error("Erro:", error);
     });
 }
